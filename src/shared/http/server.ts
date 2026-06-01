8137b1321c9b8a1282f905d7dbc7542967c0e7dc
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-06-01 15:38:45 -0300

    feat: adicionar middleware de tratamento de erros e classe AppError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import routes from "./routes";
 import cors from "cors";
+import 'express-async-errors';
+import ErrorHandleMiddleware from "../middlewares/ErrorHandleMiddleware";
 
 const app = express();
 
@@ -8,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(ErrorHandleMiddleware.haddleError);
 
 app.listen(3333, () => {
     console.log("Server is running on port 3333")
---
+++ src/shared/middlewares/ErrorHandleMiddleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errors/AppError";
+
+export default class ErrorHandleMiddleware {
+    public static haddleError(
+        error: Error,
+        _req: Request,
+        res: Response,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        _next: NextFunction,
+    ) {
+        if (error instanceof AppError) {
+            return res.status(error.statusCode).json({
+                type: "error",
+                message: error.message,
+            });
+        }
+
+        return res.status(500).json({
+            type: "error",
+            message: "Internal server error",
+        });
+
+    }
+}
