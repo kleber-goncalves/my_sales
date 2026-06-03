14e30c1ff3acfbed9ac15d7a66d1166ba9cd1b4b
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-06-03 13:59:03 -0300

    feat: add TypeORM migration for creating products table
    - Updated package.json to include migration scripts for TypeORM.
    - Modified data-source.ts to specify entities and migrations paths.
    - Created a new migration file for the products table with fields: id, name, price, quantity, created_at, and updated_at.

--- src/shared/typeorm/data-source.ts
+++ src/shared/typeorm/data-source.ts
@@ -1,4 +1,4 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import "dotenv/config";
 import { DataSource } from "typeorm";
 
@@ -7,10 +7,12 @@ const port = process.env.PORT ? Number(process.env.PORT) : 5432;
 // o "!" serve ex: "Eu garanto que esta variável vai existir no .env, podes ignorar o aviso"
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
+    type: "postgres",
     host: process.env.DB_HOST!,
     port: port,
     username: process.env.DB_USER!,
     password: process.env.DB_PASS!,
     database: process.env.DB_NAME!,
-})
+    entities: ["./src/modules/**/database/entities/*.{ts, js}"],
+    migrations: ["./src/shared/typeorm/migrations/*.{ts, js}"],
+});
