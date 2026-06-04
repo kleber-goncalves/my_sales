1d99d322ac50ee2c2dba774333631c71d228c8fe
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-06-04 19:31:11 -0300

    feat: adicionar repositório de usuários com métodos de busca por nome, id e email

---
+++ src/modules/users/database/repositories/UsersRepositories.ts
@@ -0,0 +1,20 @@
+import { AppDataSource } from "@/shared/typeorm/data-source";
+import { User } from "../entities/Users";
+
+export const usersRepositories = AppDataSource.getRepository(User).extend({
+
+    // Encontra um Usuario pelo nome
+    findByName(name: string): Promise<User | null> {
+        return this.findOneBy({ name });
+    },
+
+    // Encontra um Usuario pelo id
+    findById(id: number): Promise<User | null> {
+        return this.findOneBy({ id });
+    },
+
+    // Encontra um Usuario pelo email
+    findByEmail(email: string): Promise<User | null> {
+        return this.findOneBy({ email });
+    }
+});
