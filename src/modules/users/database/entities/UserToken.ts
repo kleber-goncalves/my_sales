73db057ba52c4cf001011a0fda7df6d498e60674
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-06-08 14:49:01 -0300

    feat: adicionar entidade e repositório para gerenciamento de tokens de usuário

---
+++ src/modules/users/database/entities/UserToken.ts
@@ -0,0 +1,28 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    Generated,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+
+
+@Entity("user_tokens")
+export class UserToken {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    @Generated("uuid")
+    token!: string;
+
+    @Column()
+    user_id!: number;
+
+    @CreateDateColumn()
+    created_at!: Date;
+
+    @UpdateDateColumn()
+    updated_at!: Date;
+}
---
+++ src/modules/users/database/repositories/UserTokensRepositories.ts
@@ -0,0 +1,21 @@
+import { AppDataSource } from "@/shared/typeorm/data-source";
+import { UserToken } from "../entities/UserToken";
+
+export const usersRepositories = AppDataSource.getRepository(UserToken).extend({
+    // Encontra um token pelo token passado como parametro e retorna ele ou null se nao encontrar nenhum token
+    async findByToken(token: string): Promise<UserToken | null> {
+        const userToken = await this.findOneBy({ token });
+        return userToken;
+    },
+
+    // Cria um novo token para o usuario passado como parametro e retorna ele criado ou undefined
+    async generate(user_id: number): Promise<UserToken | undefined> {
+        const userToken = this.create({
+            user_id,
+        });
+        await this.save(userToken);
+
+        return userToken;
+    },
+
+});
